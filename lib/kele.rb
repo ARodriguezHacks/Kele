0d85c08ee0540c1d43695a4151fff699c986463b
Angie Rodriguez <angrodri902@gmail.com>
2019-01-24 17:38:32 -0800

    Complete Initialize and Authorize KeleClient checkpoint

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,9 +1,23 @@
+require 'httparty'
+
 class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
 
-  def initialize(u, p)
-    @options = { query: { username: u, password: p } }
+  def initialize(email, password)
+    options = { body: { email: email, password: password } }
+    post_response = self.class.post('/sessions', options)
+    @auth_token = post_response['auth_token']
+
+    if @auth_token.nil?
+      puts "Invalid Email or Password. Please try Again."
+    end
+    # create an instance variable called @auth_token
+    # parse response.body with JSON.parse => ruby Hash
+    # hash will contain an auth token
+    #JSON.parse(response.body)['auth_token']
   end
 
 end
+
+# HTTParty.post('path', options)
